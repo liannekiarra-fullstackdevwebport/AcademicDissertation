b3ff42739d1eec723da64a04bea41e7da18e9475
liannepyntac <131271012+liannekiarra-fullstackdevwebport@users.noreply.github.com>
2023-05-01 13:57:29 +0100

    Add files via upload
    Documentations for the Dissertation

---
+++ LowerBounds_And_UpperBounds.py
@@ -0,0 +1,70 @@
+
+
+class RE21():
+    upperbounds = [3,3,3,3]
+    lowerbounds = [1,1.41421356.1.41421356,1]
+
+class RE22():
+    lowerbounds = [0.2,0,0]
+    upperbounds= [15,20,40]
+
+class RE23():
+    upperbounds = [100,100,200,240]
+    lowerbounds = [1,1,10,10]
+
+
+class RE24():
+    upperbounds = [4,50]
+    lowerbounds = [0.5,0.5]
+
+class RE25():
+    upperbounds = [70,3,0.5]
+    lowerbounds = [1,0.6,0.09]
+
+
+class RE31():
+    upperbounds = [100,100,3]
+    lowerbounds = [1.e-05,1.e-05, 1.e+00]
+
+class RE32():
+    upperbounds = [5,10,10,5]
+    lowerbounds = [0.125, 0.1,0.1,0.125]
+
+
+class RE33():
+    upperbounds = [80,110,3000,20]
+    lowerbounds = [55,75,1000,11]
+
+class RE34():
+    upperbounds = [3,3,3,3,3]
+    lowerbounds = [1,1,1,1,1]
+
+class RE35():
+    upperbounds = [3.6,0.8,28]
+    lowerbounds = [2.6,0.7,17,7.3,2.9,5]
+
+class RE36():
+    upperbounds = [60,60,60,60]
+    lowerbounds = [12,12,12,12]
+    
+class RE37():
+    upperbounds = [1,1,1,1]
+    lowerbounds = [0,0,0,0]
+
+class RE41():
+    upperbounds = [1.5,1.35,1.5,1.5,2.625,1.2,1.2]
+    lowerbounds = [0.5,0.45,0.5,0.5,0.875,0.4,0.4]
+
+
+class RE42():
+    upperbounds = [274.32,32.31,25, 11.71, 18,0.75]
+    lowerbounds = [150,20,13,10,14,0.63]
+
+class RE61():
+    upperbounds = [0.45,0.1,0.1]
+    lowerbounds = [0.01,0.01]
+
+
+class RE91():
+    upperbounds = [1.5, 1.35, 1.5, 1.5, 2.625, 1.2, 1.2]
+    lowerbounds = [0.5,0.45,0.5,0.5,0.875,0.4,0.4]
